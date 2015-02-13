08167f9bab8bbdced1b837ab4148f2caf3012851
jarnault <jeremy.arnault@gmail.com>
2015-02-13 11:08:48 +0100

    prout

--- src/Tremolo.java
+++ src/Tremolo.java
@@ -6,6 +6,6 @@ import Utilitaires.Midi;
 class Tremolo implements Effet{
 	public void remplirSequenceur( Molecule mol) throws InvalidMidiDataException{
 		System.out.println( "60 " + " 0 " + " 64 " +  mol.positions.get(0).temps + " " + mol.positions.get(mol.positions.size() - 1).temps*100);
-		Midi.tremolo(60, 0, 64, mol.positions.get(0).temps, mol.positions.get(mol.positions.size() - 1).temps*100);
+		Midi.tremolo(60, 0, 64, mol.positions.get(0).temps, mol.positions.get(mol.positions.size() - 1).temps*1000);
 	}
 }
