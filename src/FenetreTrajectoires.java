415dcdea2381ef72ea45ede755613be65b4475ab
debian <debian@debian>
2015-03-04 06:43:04 -0600

    FenetreTrajectoire marche pas

--- src/FenetreTrajectoires.java
+++ src/FenetreTrajectoires.java
@@ -9,12 +9,36 @@ import java.util.ArrayList;*/
 import java.util.*;
 
 public class FenetreTrajectoires extends JApplet {
+
+	private ArrayList<Molecule> molecules;
+	
+	public void FenetreTrajectoires(ArrayList<Molecule> mols){
+		setMols(mols);
+		this.init();
+		
+	}
+	
+
+	
    public void init() {
+       JFrame f = new JFrame("Line");
+      f.addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent e) {
+            System.exit(0);
+         }
+      });
+      JApplet applet = new FenetreTrajectoires();
+      f.getContentPane().add("Center", applet);
+      applet.init();
+      f.pack();
+      f.setSize(new Dimension(1000, 1000));
+      f.setVisible(true);
+      
       setBackground(Color.white);
       setForeground(Color.white);
    }
-   public void paint(Graphics g, ArrayList<Molecule> molecules) {
-
+   public void paint(Graphics g) {
+			ArrayList<Molecule> molecules= this.getMols();
       Graphics2D g2 = (Graphics2D) g;
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
       RenderingHints.VALUE_ANTIALIAS_ON);
@@ -37,22 +61,20 @@ public class FenetreTrajectoires extends JApplet {
 	   		 }
 	   		 
 			}
-      
+     public static void main(String[] args){
+      FenetreTrajectoires  ft = new FenetreTrajectoires();  
+      //FenetreTrajectoires.setVisible(true);
+   }
+
       
   
-   public static void main(String s[]) {
-      JFrame f = new JFrame("Line");
-      f.addWindowListener(new WindowAdapter() {
-         public void windowClosing(WindowEvent e) {
-            System.exit(0);
-         }
-      });
-      JApplet applet = new FenetreTrajectoires();
-      f.getContentPane().add("Center", applet);
-      applet.init();
-      f.pack();
-      f.setSize(new Dimension(300, 300));
-      f.setVisible(true);
-   }
+   	public ArrayList<Molecule> getMols()
+	{
+		return this.molecules;
+	}
+	
+	public void setMols(ArrayList<Molecule> mols){
+		molecules=mols;
+	}
    
 }
