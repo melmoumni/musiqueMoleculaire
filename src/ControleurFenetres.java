ea69f5966cdd09a53d954d0b02b82c6de21218d9
iangelet <isanze@hotmail.fr>
2015-03-04 14:43:25 +0100

    Interface - lien entre les parametres et le tableau chercheur

---
+++ src/ControleurFenetres.java
@@ -0,0 +1,76 @@
+
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class ControleurFenetres {
+	static FenetreParametres params;
+	static JFrame fenetrePrincipale ;
+	static float alpha1,alpha2,alpha3;
+	static String noteRef;
+	static int duree;
+	static int largeur, hauteur;
+	static String fichierTrajectoire, fichierMvts;
+	
+	static Controleur controleur;
+	static Parseur p = new Parseur();
+	
+	public ControleurFenetres(){
+		params = new FenetreParametres();
+		params.setVisible(true);
+	}
+	
+	/* Choix de l'utilisateur : 
+	 * 	Lance la fenetre correspondant à l'utilisateur
+	 */
+	public static void choixUtilisateur(){
+		System.out.println(params.getUtilisateur());
+		if(params.getUtilisateur().compareTo("chercheur")==0){
+			params.dispose(); // fermeture de la fenetre parametre
+			controleur = new Chercheur();
+			parser();
+			fenetrePrincipale = new TableauChercheurVue(true,null);
+			fenetrePrincipale.setVisible(true);
+		}
+		else if(params.getUtilisateur().compareTo("compositeur")==1){
+			// to do
+		}
+	}
+	
+	private static void parser(){
+		try {
+			p.lireFichierAnalyse(fichierMvts);
+		}
+		catch (IOException e) {}
+		try {	
+			p.lireFichierTrajectoire(fichierTrajectoire);
+		}
+		catch (IOException e) {}
+		try {	
+			p.lectureTimbre("./data/listeInstruments.txt");
+		}
+		catch (IOException e) {}
+		controleur.initMolecules(p);
+	}
+	
+	/* Récupère les différents paramêtres renseignées par l'utilisateur dans la fenetre de parametre */
+	public static void recupereParametre(){
+		alpha1 = params.getValeurAlpha1();
+		alpha2 = params.getValeurAlpha2();
+		alpha3 = params.getValeurAlpha3();
+		noteRef = params.getNoteRef();
+		duree = params.getDuree();
+		largeur = params.getLargeur();
+		hauteur = params.getHauteur();
+		fichierTrajectoire = params.getFilenameT();
+		System.out.println(fichierTrajectoire);
+		fichierMvts = params.getFilenameM();
+		System.out.println(fichierMvts);
+	}
+
+	
+	public static void main(String[] args) {
+		ControleurFenetres vue = new ControleurFenetres();
+	}
+}
